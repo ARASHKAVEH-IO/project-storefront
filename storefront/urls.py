eccfe892bf3d643552002d15cc9df8db8162ded6
ARASH KAVEH <arashkaveh@gmail.com>
2025-08-10 13:58:02 +0330

    initial commit

--- storefront/urls.py
+++ storefront/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
+from django.conf import settings
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('playground/' , include('playground.urls'))
+    path('playground/', include('playground.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += debug_toolbar_urls()
+
