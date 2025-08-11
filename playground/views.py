25d6b8acdcf04bb4f3b47ec7f93b1edac81e927f
ARASH KAVEH <arashkaveh@gmail.com>
2025-08-11 18:03:52 +0330

    initial commit

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 
 
 def say_hello(request):
-    query_set = Product.objects.all()
-
-    return render(request , 'hello.html' , {'name' : 'Arash'})
+    
+    querysete = Product.objects.filter(unit_price__range=(20 , 30))
+    
+    return render(request , 'hello.html' , {'name' : 'Arash' , 'products' : list(queryset)})
