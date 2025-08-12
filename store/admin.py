47b791fa69b21b6aba577a8ecb5b282cfae881e9
ARASH KAVEH <arashkaveh@gmail.com>
2025-08-12 14:34:55 +0330

    adding customer to admin pannel

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name' , 'last_name' , 'membership']
+    list_editable = ['membership' ,]
+    ordering = ['first_name' , 'last_name']
+    list_per_page = 5
+
+
+
