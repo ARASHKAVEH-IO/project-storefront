086678c540410e249a8e2a9037c7158a4f4175d2
ARASH KAVEH <arashkaveh@gmail.com>
2025-08-12 14:00:26 +0330

    admin pannel update

--- storefront/urls.py
+++ storefront/urls.py
@@ -19,6 +19,10 @@ from django.urls import path, include
 from django.conf import settings
 from debug_toolbar.toolbar import debug_toolbar_urls
 
+admin.site.site_header = 'STOREFRONT ADMIN'
+admin.site.index_title = 'Admin'
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls')),
