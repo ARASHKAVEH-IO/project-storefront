2c3caa72450acc47b028ea62b2c341e1cbb94e7c
ARASH KAVEH <arashkaveh@gmail.com>
2025-08-12 12:30:21 +0330

    initial commit

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-
 from store.models import Customer
 
 
 def say_hello(request):
-     customer = Customer.objects.filter(first_name__icontains = "a")
+     
+     customer = Customer.objects.order_by('first_name' , 'last_name')
     
      return render(request , 'hello.html' , {'name' : 'Arash' ,'customers': list(customer)})
